d35af7960ed72e0ca023deb312cf0303d87ca20c
Huzaifa-Rana <huzaifarana.pk@gmail.com>
2025-07-24 23:06:06 +0500

    updated errors

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RoleSeeder extends Seeder
 {
